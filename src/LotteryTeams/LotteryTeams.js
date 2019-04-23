8c430665727b78a8d887073a0227d8ef2d2478f0
Maxwell Todd <mtmaxtodd@gmail.com>
2019-04-23 16:56:52 -0500

    Removed state from the lotteryorder component.

--- src/LotteryTeams/LotteryTeams.js
+++ src/LotteryTeams/LotteryTeams.js
@@ -4,59 +4,37 @@ import LotteryTeamRow from './LotteryTeamRow';
 import SimLotteryButton from '../SimLotteryButton/SimLotteryButton';
 
 class LotteryTeams extends Component {
-constructor(props){
-    super(props)
-    this.state = {
-        lotteryTeams:
-            [{name: "New York", seed: 1, place: 1},
-            { name: "Cleveland", seed: 2, place: 2 },
-            { name: "Phoenix", seed: 3, place: 3 },
-            { name: "Chicago", seed: 4, place: 4 },
-            { name: "Atlanta", seed: 5, place: 5 },
-            { name: "Washington", seed: 6, place: 6 },
-            { name: "New Orleans", seed: 7, place: 7 },
-            { name: "Memphis", seed: 8, place: 8 },
-            { name: "Atlanta", seed: 9, place: 9 },
-            { name: "Minnesota", seed: 10, place: 10 },
-            { name: "Los Angeles", seed: 11, place: 11 },
-            { name: "Charlotte", seed: 12, place: 12 },
-            { name: "Miami", seed: 13, place: 13 },
-            { name: "Boston", seed: 14, place: 14 }],
-    }   
-}
+    constructor(props) {
+        super(props)
+    }
 
     componentDidMount() {
         let action = { type: 'ORIGINAL_ORDER' }
         this.props.dispatch(action);
     }
 
-
     render() {
         return (
             <div>
-            <div>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Place</th>
-                        <th>Team</th>
-                    </tr>
-                </thead>
-                <tbody>   
-                            {this.props.reduxStore.draftLotteryOrder.map(team=>{
-                             return   <tr>
+                <div>
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Place</th>
+                                <th>Team</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {this.props.reduxStore.draftLotteryOrder.map(team => {
+                                return <tr>
                                     <td>{team.place}</td>
                                     <td>{team.name}</td>
                                 </tr>
                             })}
-                        {/* {this.state.lotteryTeams.map(team =>
-                            <LotteryTeamRow team={team}/>
-                        )} */}
-                </tbody>
-            </table>
-            {JSON.stringify.reduxStore}
-            </div>
-            <SimLotteryButton lotteryTeams = {this.state.lotteryTeams}/>
+                        </tbody>
+                    </table>
+                </div>
+                <SimLotteryButton />
             </div>
         )
     }
