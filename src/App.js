de6c9c2416e53e261c11535e82c0a84c63fe0d02
Maxwell Todd <mtmaxtodd@gmail.com>
2019-05-01 14:24:43 -0500

    Added a div to seperate the two wolves images on either side of the table.

--- src/App.js
+++ src/App.js
@@ -13,11 +13,15 @@ class App extends Component {
   render() {
     let leftImage = "courtLeft";
     let rightImage = "courtRight";
+    let oldWolves;
+    let newWolves;
     this.props.reduxStore.draftLotteryOrder.map(team => {
       if ((team.name == "Minnesota") && (team.place == 1 || team.place == 2 || team.place == 3
         || team.place == 4)) {
         rightImage = "karlSmirk";
         leftImage = "karlBBQ";
+        oldWolves = <div className = "oldWolves"></div>;
+        newWolves = <div className = "newWolves"></div>
       }
     })
     return (
@@ -30,14 +34,18 @@ class App extends Component {
           alignItems="flex-start"
         >
           <Grid item xs>
+            {newWolves}
         <div className={leftImage}></div>
+        
         </Grid>
         {/* <SimLotteryButton /> */}
             <Grid item xs>
         <LotteryTeams /> 
               </Grid>
               <Grid item xs>
+            {oldWolves}
           <div className={rightImage}></div> 
+          
               </Grid>
         </Grid> 
       </div>
