f8361186bbb5d0cde7b076ea0db1621807f208e8
Maxwell Todd <mtmaxtodd@gmail.com>
2019-05-02 16:50:32 -0500

    Added Nav component and style sheet and will tailor it to my app's needs. Installed readct router dom into the package json. Modified the app.js to have routes and to show the home compoent as default.

--- src/App.js
+++ src/App.js
@@ -1,53 +1,24 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import './App.css';
+import { HashRouter as Router, Route, Redirect, Switch,} from 'react-router-dom';
+import Home from './Home/Home.js';
 import './index.css';
-import LotteryTeams from './LotteryTeams/LotteryTeams.js';
-// import SimLotteryButton from './SimLotteryButton/SimLotteryButton.js';
-import Header from './Header/Header.js';
-import Grid from '@material-ui/core/Grid';
 class App extends Component {
   constructor(){
     super()
   }
   render() {
-    let leftImage = "courtLeft";
-    let rightImage = "courtRight";
-    let oldWolves;
-    let newWolves;
-    this.props.reduxStore.draftLotteryOrder.map(team => {
-      if ((team.name == "Minnesota") && (team.place == 1 || team.place == 2 || team.place == 3
-        || team.place == 4)) {
-        rightImage = "karlSmirk";
-        leftImage = "karlBBQ";
-        oldWolves = <div className = "oldWolves"></div>;
-        newWolves = <div className = "newWolves"></div>
-      }
-    })
     return (
       <div >
-        <Header/>
-        <Grid className="container"
-          container
-          direction="row"
-          justify="space-between"
-          alignItems="flex-start"
-        >
-          <Grid item xs>
-            {newWolves}
-        <div className={leftImage}></div>
-        
-        </Grid>
-        {/* <SimLotteryButton /> */}
-            <Grid item xs>
-        <LotteryTeams /> 
-              </Grid>
-              <Grid item xs>
-            {oldWolves}
-          <div className={rightImage}></div> 
-          
-              </Grid>
-        </Grid> 
+        <Router>
+          <Switch>
+            <Redirect exact from="/" to="/home" />
+            <Route 
+              exact path = "/home"
+              component = {Home}/>
+            <Route render={() => <h1>404</h1>} />
+          </Switch>
+        </Router>
       </div>
     );
   }
---
+++ src/Nav/Nav.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import LogOutButton from '../LogOutButton/LogOutButton';
+import './Nav.css';
+
+const Nav = (props) => (
+
+    <div className="nav"
+    >
+        <Link to="/home">
+            <h2 className="nav-title">Daily Orders App</h2>
+        </Link>
+
+        <div className="nav-right"
+
+        >
+            <Link className="nav-link" to="/home">
+                {/* Show this link if they are logged in or not,
+        but call this link 'Home' if they are logged in,
+        and call this link 'Login / Register' if they are not */}
+                {props.user.id ? <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="none" d="M0 0h24v24H0V0z" /><path d="M12 5.69l5 4.5V18h-2v-6H9v6H7v-7.81l5-4.5M12 3L2 12h3v8h6v-6h2v6h6v-8h3L12 3z" /></svg> : 'Login'}
+
+            </Link>
+            {/* Show the link to the info page and the logout button if the user is logged in */}
+            {props.user.id && (
+                <>
+                    <Link className="nav-link" to="/history">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="none" d="M0 0h24v24H0V0z" /><path d="M7 11h2v2H7v-2zm14-5v14c0 1.1-.9 2-2 2H5c-1.11 0-2-.9-2-2l.01-14c0-1.1.88-2 1.99-2h1V2h2v2h8V2h2v2h1c1.1 0 2 .9 2 2zM5 8h14V6H5v2zm14 12V10H5v10h14zm-4-7h2v-2h-2v2zm-4 0h2v-2h-2v2z" /></svg>
+                    </Link>
+
+
+                </>
+            )}
+            {props.user.manager && (
+                <Link className="nav-link" to="/update">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="none" d="M0 0h24v24H0V0z" /><path d="M19 3h-4.18C14.4 1.84 13.3 1 12 1s-2.4.84-2.82 2H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm-7-.25c.22 0 .41.1.55.25.12.13.2.31.2.5 0 .41-.34.75-.75.75s-.75-.34-.75-.75c0-.19.08-.37.2-.5.14-.15.33-.25.55-.25zM19 19H5V5h14v14zM12 6c-1.65 0-3 1.35-3 3s1.35 3 3 3 3-1.35 3-3-1.35-3-3-3zm0 4c-.55 0-1-.45-1-1s.45-1 1-1 1 .45 1 1-.45 1-1 1zm-6 6.47V18h12v-1.53c0-2.5-3.97-3.58-6-3.58s-6 1.07-6 3.58zM8.31 16c.69-.56 2.38-1.12 3.69-1.12s3.01.56 3.69 1.12H8.31z" /></svg>
+                </Link>
+            )}
+            {props.user.id && (
+                <LogOutButton className="nav-link" />
+            )}
+        </div>
+    </div>
+);
+
+const mapStateToProps = reduxStore => ({
+    reduxStore,
+});
+
+export default connect(mapStateToProps)(Nav);
