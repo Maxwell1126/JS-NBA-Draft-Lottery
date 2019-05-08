9177735c15e271a8d430fb44dbb95197ae134358
Maxwell Todd <mtmaxtodd@gmail.com>
2019-05-08 15:12:30 -0500

    Added dependencies to the package json. Added a server file to the project.

--- Database.sql
+++ Database.sql
@@ -9,5 +9,6 @@ VALUES('New York', 1),('Cleveland', 2),('Phoenix', 3),('Chicago', 4),('Atlanta',
 
 INSERT INTO "stats"("name")
 VALUES('% First'),('% Second'),('% Third'),('% Fourth'),('% Top Four'),('Mean Place');
-CREATE TABLE "simulaitons"("id" serial PRIMARY KEY, "1st" INTEGER, FOREIGN KEY("1st") REFERENCES "teams"("id"),
+
+CREATE TABLE "simulations"("id" serial PRIMARY KEY, "1st" INTEGER, FOREIGN KEY("1st") REFERENCES "teams"("id"),
 "2nd" INTEGER, FOREIGN KEY("2nd") REFERENCES "teams"("id"), "3rd" INTEGER, FOREIGN KEY("3rd") REFERENCES "teams"("id"), "4th" INTEGER, FOREIGN KEY("4th") REFERENCES "teams"("id"), "5th" INTEGER, FOREIGN KEY("5th") REFERENCES "teams"("id"), "6th" INTEGER, FOREIGN KEY("6th") REFERENCES "teams"("id"), "7th" INTEGER, FOREIGN KEY("7th") REFERENCES "teams"("id"), "8th" INTEGER, FOREIGN KEY("8th") REFERENCES "teams"("id"), "9th" INTEGER, FOREIGN KEY("9th") REFERENCES "teams"("id"), "10th" INTEGER, FOREIGN KEY("10th") REFERENCES "teams"("id"), "11th" INTEGER, FOREIGN KEY("11th") REFERENCES "teams"("id"), "12th" INTEGER, FOREIGN KEY("12th") REFERENCES "teams"("id"), "13th" INTEGER, FOREIGN KEY("13th") REFERENCES "teams"("id"), "14th" INTEGER, FOREIGN KEY("14th") REFERENCES "teams"("id"));
---
+++ Server/Server.js
@@ -0,0 +1,33 @@
+
+const express = require('./node_modules/express');
+require('./node_modules/dotenv').config();
+
+const app = express();
+const bodyParser = require('./node_modules/body-parser');
+const sessionMiddleware = require('./modules/session-middleware');
+
+
+
+// Route includes
+const userRouter = require('./routes/user.router');
+
+// Body parser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Passport Session Configuration //
+app.use(sessionMiddleware);
+
+/* Routes */
+app.use('/api/user', userRouter);
+
+// Serve static files
+app.use(express.static('build'));
+
+// App Set //
+const PORT = process.env.PORT || 5000;
+
+/** Listen * */
+app.listen(PORT, () => {
+    console.log(`Listening on port: ${PORT}`);
+});
