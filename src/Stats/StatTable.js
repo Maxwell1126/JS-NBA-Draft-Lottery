89451c474b40fcab984b645fd0fb7dab1840c2c2
Maxwell Todd <mtmaxtodd@gmail.com>
2019-05-09 15:53:36 -0500

    Adjusted spacing for the table and the text font for the simulation counter.

--- src/Stats/StatTable.js
+++ src/Stats/StatTable.js
@@ -35,7 +35,7 @@ class StatsTable extends Component {
                 direction="column"
                 justify="center"
                 alignItems="center">
-                <Grid item xs >
+                <Grid item xs className = "counter">
                 <h3>Simulated {counter} Times</h3>
                 </Grid>
                 <Table item xs >
