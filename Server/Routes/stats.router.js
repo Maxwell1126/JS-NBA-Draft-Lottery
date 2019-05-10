0dc5394cc2f243b3e6cbf19e6af391ceb721776e
Maxwell Todd <mtmaxtodd@gmail.com>
2019-05-09 23:25:00 -0500

    Cleaned up the FinalPlaceReducer. Added a get route to the stats route to get the count of times each team appears in the top four.

--- Server/Routes/stats.router.js
+++ Server/Routes/stats.router.js
@@ -10,5 +10,42 @@ router.get('/', (req, res) => {
         console.log('error in server getting from database.', error);
     })
 });
+
+router.get('/topFour', (req, res) => {
+    (async () => {
+        const client = await pool.connect();
+        try {
+            await client.query('BEGIN');
+
+            let simulationCheck = `SELECT * FROM "simulations" ORDER BY ID DESC LIMIT 1;`;
+            let results = await client.query(simulationCheck);
+            if (results.rows.length == 0) {
+               gooseEgg = 0;
+               res.send(gooseEgg)
+            } else {
+                topFourCounts = [];
+                let getTeams = `SELECT "id" FROM "teams;`;
+                let teams = await client.query(getTeams);
+                for(let i = 0; o <teams.rows.length; i++){
+                    let getTopFour = `SELECT COUNT(*) FROM "simulations_results" WHERE "team_id" = ($1)
+                                      AND ("place" = 1 OR "place" = 2 OR "place" = 3 OR "place" = 4);`;
+                    let teamId = [teams.rows[i]];
+                    let teamCount = await client.query(getTopFour, teamId);
+                    topFourCounts.push({id: teams.rows[i], count: teamCount.rows[0].count})
+                }
+                res.send(topFourCounts);
+            }
+        } catch (e) {
+            console.log('ROLLBACK', e);
+            await client.query('ROLLBACK');
+            throw e;
+        } finally {
+            client.release();
+        }
+    })().catch((error) => {
+        console.log('error in server posting to database.', error);
+        res.sendStatus(500);
+    })
+});
     
 module.exports = router;
--- src/Redux/Reducers/FinalPlaceReducer.js
+++ src/Redux/Reducers/FinalPlaceReducer.js
@@ -13,43 +13,15 @@ const DEFAULT_ORDER =
     { name: "Charlotte", seed: 12, place: 12 },
     { name: "Miami", seed: 13, place: 13 },
     { name: "Boston", seed: 14, place: 14 }];
-let draftOrder =
-    [{ name: "New York", seed: 1, place: 1 },
-    { name: "Cleveland", seed: 2, place: 2 },
-    { name: "Phoenix", seed: 3, place: 3 },
-    { name: "Chicago", seed: 4, place: 4 },
-    { name: "Atlanta", seed: 5, place: 5 },
-    { name: "Washington", seed: 6, place: 6 },
-    { name: "New Orleans", seed: 7, place: 7 },
-    { name: "Memphis", seed: 8, place: 8 },
-    { name: "Atlanta", seed: 9, place: 9 },
-    { name: "Minnesota", seed: 10, place: 10 },
-    { name: "Los Angeles", seed: 11, place: 11 },
-    { name: "Charlotte", seed: 12, place: 12 },
-    { name: "Miami", seed: 13, place: 13 },
-    { name: "Boston", seed: 14, place: 14 }];
 const draftLotteryOrder = (state = DEFAULT_ORDER, action) => {
     switch (action.type) {
         case 'SET_ORDER':
-        let latestOrder = action.payload;
+            let latestOrder = action.payload;
             console.log('database response', action.payload);
-            // for (let i = 0; i < action.payload.length; i++) {
-            //     for (let j = 0; j < draftOrder.length; j++) {
-            //         if (draftOrder[j].seed == action.payload[i].team_id) {
-            //             draftOrder[j].place = action.payload[i].place;
-            //         }
-            //     }
-            // }
-            // function propComparator(prop) {
-            //     return function (a, b) {
-            //         return a[prop] - b[prop];
-            //     }
-            // }
-            // draftOrder = draftOrder.sort(propComparator('place'));
             for (let i = 0; i < latestOrder.length; i++) {
                 if (latestOrder[i].seed == 9 && latestOrder[i].place < 5) {
                     latestOrder[i].name = "Dallas";
-                } else if (latestOrder[i].seed== 9 && latestOrder[i].place > 4) {
+                } else if (latestOrder[i].seed == 9 && latestOrder[i].place > 4) {
                     latestOrder[i].name = "Atlanta";
                 } else if (latestOrder[i].seed == 8 && latestOrder[i].place > 8) {
                     latestOrder[i].name = "Boston";
@@ -63,8 +35,6 @@ const draftLotteryOrder = (state = DEFAULT_ORDER, action) => {
             }
             console.log('draft order', latestOrder);
             state = latestOrder
-            return state;
-        case 'DRAFT_ORDER':
             return state;
         case 'ORIGINAL_ORDER':
             return DEFAULT_ORDER;
