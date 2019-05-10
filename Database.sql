f390d9e86a2ea9cca22a92b25e845fabb9d2849f
Maxwell Todd <mtmaxtodd@gmail.com>
2019-05-09 19:02:43 -0500

    Changed the database specifically the teams and simulations table's columns.

--- Database.sql
+++ Database.sql
@@ -1,14 +1,13 @@
-CREATE TABLE "teams"("id" serial PRIMARY KEY, "name" VARCHAR(20), "seed" INTEGER);
-
+CREATE TABLE "teams"("id" serial PRIMARY KEY, "name" VARCHAR(20));
 CREATE TABLE "stats"("id" serial PRIMARY KEY, "name" VARCHAR(20));
 
-INSERT INTO "teams"("name", "seed")
-VALUES('New York', 1),('Cleveland', 2),('Phoenix', 3),('Chicago', 4),('Atlanta', 5),('Washington', 6),
-('New Orleans', 7),('Memphis', 8),('Dallas', 9),('Minnesota', 10),('Los Angeles', 11),('Charlotte', 12),
-('Miami', 13),('Boston', 14);
+INSERT INTO "teams"("name")
+VALUES('New York'),('Cleveland'),('Phoenix'),('Chicago'),('Atlanta'),('Washington'),
+('New Orleans'),('Memphis'),('Dallas'),('Minnesota'),('Los Angeles'),('Charlotte'),
+('Miami'),('Boston');
 
 INSERT INTO "stats"("name")
 VALUES('% First'),('% Second'),('% Third'),('% Fourth'),('% Top Four'),('Mean Place');
 
-CREATE TABLE "simulations"("id" serial PRIMARY KEY, "1st" INTEGER, FOREIGN KEY("1st") REFERENCES "teams"("id"),
-"2nd" INTEGER, FOREIGN KEY("2nd") REFERENCES "teams"("id"), "3rd" INTEGER, FOREIGN KEY("3rd") REFERENCES "teams"("id"), "4th" INTEGER, FOREIGN KEY("4th") REFERENCES "teams"("id"), "5th" INTEGER, FOREIGN KEY("5th") REFERENCES "teams"("id"), "6th" INTEGER, FOREIGN KEY("6th") REFERENCES "teams"("id"), "7th" INTEGER, FOREIGN KEY("7th") REFERENCES "teams"("id"), "8th" INTEGER, FOREIGN KEY("8th") REFERENCES "teams"("id"), "9th" INTEGER, FOREIGN KEY("9th") REFERENCES "teams"("id"), "10th" INTEGER, FOREIGN KEY("10th") REFERENCES "teams"("id"), "11th" INTEGER, FOREIGN KEY("11th") REFERENCES "teams"("id"), "12th" INTEGER, FOREIGN KEY("12th") REFERENCES "teams"("id"), "13th" INTEGER, FOREIGN KEY("13th") REFERENCES "teams"("id"), "14th" INTEGER, FOREIGN KEY("14th") REFERENCES "teams"("id"));
+CREATE TABLE "simulations"("id" serial PRIMARY KEY,
+    "team_id" INTEGER, FOREIGN KEY("team_id") REFERENCES "teams"("id"), "place" INTEGER);
--- Server/Routes/simulations.router.js
+++ Server/Routes/simulations.router.js
@@ -3,11 +3,11 @@ const pool = require('../Pool');
 const router = express.Router();
 
 router.post('/', (req, res) => {
-    console.log(req.body[0].seed)
+    console.log(req.body[0])
     let simulation = `INSERT INTO "simulations" ("1st", "2nd", "3rd", "4th", "5th", "6th", "7th",
                                                 "8th", "9th", "10th", "11th", "12th", "13th", "14th")
                       VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14);`;
-    let values = [req.body[0].seed, req.body[1].seed, req.body[2].seed, req.body[3].seed,
+    let values = [req.body[0], req.body[1], req.body[2], req.body[3],
         req.body[4].seed, req.body[5].seed, req.body[6].seed, req.body[7].seed, req.body[8].seed, 
         req.body[9].seed, req.body[10].seed, req.body[11].seed, req.body[12].seed, req.body[13].seed];
     pool.query(simulation, values).then((response) => {
