89e40e5eae1eba4cbe9fcaa03f789578dd5a10e1
Maxwell Todd <mtmaxtodd@gmail.com>
2019-05-11 18:53:50 -0500

    Created a reducer to hold the total number of simulations.

--- src/Redux/Reducers/index.js
+++ src/Redux/Reducers/index.js
@@ -2,10 +2,13 @@ import { combineReducers } from 'redux';
 import draftLotteryOrder from './FinalPlaceReducer.js';
 import stats from './statsReducer.js';
 import selectedStat from './selectedStat.js';
+import totalSims from './totalSimsReducer.js';
+
 const rootReducer = combineReducers({
     draftLotteryOrder,
     stats,
     selectedStat,
+    totalSims,
 });
 
 export default rootReducer;
---
+++ src/Redux/Reducers/totalSimsReducer.js
@@ -0,0 +1,14 @@
+const DEFAULT_STATE= 0;
+
+const totalSims = (state = DEFAULT_STATE, action) => {
+    switch (action.type) {
+        case 'SET_TOTAL_SIMS':
+            state = action.payload;
+            return state;
+        default:
+            return state;
+
+    }
+};
+
+export default totalSims;
