32bef11aac878dd9097788ccaf03fdacff5e89d3
Maxwell Todd <mtmaxtodd@gmail.com>
2019-05-12 20:27:38 -0500

    Added build folder for Heroku.

---
+++ build/precache-manifest.8927dff4b56f9c0c9dad8fa598131378.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "92eddb1d103b0cd0b0aa",
+    "url": "/static/css/main.7bee291f.chunk.css"
+  },
+  {
+    "revision": "92eddb1d103b0cd0b0aa",
+    "url": "/static/js/main.d1f3a035.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "89d20b9f44023790393c",
+    "url": "/static/js/2.4b686174.chunk.js"
+  },
+  {
+    "revision": "a8041d057479e7d7804abcefbdedbcbd",
+    "url": "/static/media/ja_morant.a8041d05.jpg"
+  },
+  {
+    "revision": "97d3bd330d624ea91255740491289b1d",
+    "url": "/static/media/zionWolves.97d3bd33.png"
+  },
+  {
+    "revision": "bf9da252eb9090ff06970f23d2a27450",
+    "url": "/static/media/oldWolves.bf9da252.png"
+  },
+  {
+    "revision": "0adda28702c02cb5c0459e3fa3d72f75",
+    "url": "/static/media/new_wolves.0adda287.png"
+  },
+  {
+    "revision": "2c1337a13a904893db75a552e0e0d27a",
+    "url": "/static/media/karlBBQ.2c1337a1.jpg"
+  },
+  {
+    "revision": "ffa79f8e74a8eb9c7cb3a5a1f95ea06d",
+    "url": "/static/media/karlSmirk.ffa79f8e.jpg"
+  },
+  {
+    "revision": "c2995628cf94465dd73e855c0b2d85e7",
+    "url": "/static/media/Court_Right.c2995628.png"
+  },
+  {
+    "revision": "92d4df74d6480c12ffa2df2e2d3aeec1",
+    "url": "/static/media/Court_Left.92d4df74.png"
+  },
+  {
+    "revision": "3ffccaea588504fd06b12dc438e3f2df",
+    "url": "/index.html"
+  }
+];
