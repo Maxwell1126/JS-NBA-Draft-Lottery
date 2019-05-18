b6bb715ee39a6313c0bc3a8d60509a2be6f7a30d
Maxwell Todd <mtmaxtodd@gmail.com>
2019-05-17 20:59:13 -0500

    Added content to the Info page.

--- src/Info/Info.js
+++ src/Info/Info.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import './Home.css';
+import '../Home/Home.css';
 import '../index.css';
 import Grid from '@material-ui/core/Grid';
 class Info extends Component {
@@ -35,6 +35,27 @@ class Info extends Component {
                     </Grid>
                     <Grid item xs>
                         <div>
+                        
+                        <ul>
+                                <h1>Draft Lottery Description</h1>
+                            <li>
+                                The Draft Lottery is a random drawing between the non-playoff teams of the NBA to determine who
+                                gets the right to picks one through four. Each team gets a different percent of chance to win the lottery based
+                                on how poorly they performed.
+                            </li>
+                            <li>
+                                The NBA is comprosied of thirty teams and has two conferences, the East and West.
+                                Eight teams from each conference qualify for the postseason playoffs based on their records.
+                                The remaining seven teams from each conference are ordered by their record, 
+                                with the worst team being first, to comprise the Draft Lottery pool.
+                            </li>
+                            <li>
+                                The lottery begins by determining who gets the first overall pick. a random number is drawn and who ever
+                                owns the number then owns the first overall pick. The team the number belonged to can no longer win any other picks.
+                                This process is then repeated for picks two through three. Once the top four picks have been determined, the remaining
+                                ten teams are given picks five through fourteen in order of their record with the worst team getting the fifth pick.
+                            </li>
+                        </ul>
                         </div>
                     </Grid>
                     <Grid item xs>
